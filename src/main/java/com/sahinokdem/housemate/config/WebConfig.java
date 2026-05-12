d284d650d75b307ee24083b13a8a9ba777760cdf
sahin <sahinokdem@gmail.com>
2026-05-12 22:46:59 +0300

    Nginx: Allow local fe

--- src/main/java/com/sahinokdem/housemate/config/WebConfig.java
+++ src/main/java/com/sahinokdem/housemate/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.sahinokdem.housemate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Senin lokal Vite adresin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
